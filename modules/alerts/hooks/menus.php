981d506aa124ac509f4990c484cf5471df2a1d1a
tuxmonkey <tuxmonkey@3e232514-370a-0410-8a2c-e2cb0e19c5b9>
2006-02-14 05:17:31 +0000

    restructure of alerts module

--- modules/alerts/hooks/menus.php
+++ modules/alerts/hooks/menus.php
@@ -1,20 +1,20 @@
 <?php
 /* $Id: menus.php 5 2004-08-10 01:30:40Z eroberts $ */
 /**
- * @package Issue-Tracker
- * @subpackage Alerts
- */
+* @package Issue-Tracker
+* @subpackage Alerts
+*/
 if (preg_match('/'.basename(__FILE__).'/',$_SERVER['PHP_SELF'])) {
 	print('Direct module access forbidden.');
 	exit;
 }
 if ($_SESSION['group_count'] > 0) {
-	$leftnav_menu['Alerts'] = array(
+	$_ENV['menu']['Alerts'] = array(
 		'url' => '?module=alerts',
 		'sub' => array()
 	);
 	if (permission_check('create_alerts')) {
-		$leftnav_menu['Alerts']['sub']['New Alert'] = '?module=alerts&action=new';
+		$_ENV['menu']['Alerts']['sub']['New Alert'] = '?module=alerts&action=new';
 	}
 }
 ?>
